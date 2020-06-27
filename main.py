1d356d1824097dce22cfefe5ab8d75c1e5488adf
SamuelSoon212 <66506151+SamuelSoon212@users.noreply.github.com>
2020-06-27 22:40:21 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+angle_change = 1
+length = 5
+depth = 0
+speed(99999999)
+pensize(2)
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+while depth < 300:
+    pencolor(colors[depth%7])
+    depth += 1
+    right(91)
+    forward(length + depth*3)
+
+
+
+mainloop()
+
